83481b9ffd287bcdb96332e53384cbd9bd8e13dc
chaizhize <chaizhize>
2022-11-25 09:19:32 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Hello Vue 3.0 + Vite" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import HelloWorld from "./components/HelloWorld.vue";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: "App",
+		components: {
+			HelloWorld,
+		},
+	};
 </script>
+<style>
+	img {
+		width: 300px;
+		font-size: 30px;
+		height: 100px;
+	}
+</style>
