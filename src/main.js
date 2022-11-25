9121f407fa7f1073f30e145271548efb4402c175
LiuRan <2309665490@qq.com>
2022-11-25 22:54:36 +0800

    搜索关键字列表页开发

--- src/main.js
+++ src/main.js
@@ -1,15 +1,22 @@
 import { createApp } from "vue";
 //element-plus插件:完整引入
 import ElementPlus from 'element-plus'
-import zhCn from 'element-plus/es/locale/lang/zh-cn'
+
+import zhCn from 'element-plus/es/locale/lang/zh-cn';
+import * as Elicons from "@element-plus/icons-vue";
+
 import 'element-plus/dist/index.css'
+
 import "./index.css";
 import App from "./App.vue";
 import "reset-css";
 import router from './router'
 
+const app = createApp(App);
+for (const name in Elicons) {
+  app.component(name, Elicons[name]);
+}
 
-createApp(App)
-.use(router)
+app.use(router)
 .use(ElementPlus, { size: 'small', zIndex: 3000, locale: zhCn, })
   .mount("#app");
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="search-container">
+      <div class="search-container-content">
+
+        <!-- 搜索输入框 -->
     <div class="search-input">
       <el-input
         v-model.enter="searchVal"
@@ -7,10 +10,13 @@
         :placeholder="placeholderText"
         :prefix-icon="Search"
         @keyup.enter.native="searcHistory"
+        @input="handleSearchInput"
         clearable
       />
       <span>取消</span>
     </div>
+    <div v-show="!searchVal">
+    <!-- 搜索历史记录 -->
     <div class="search-history" v-if="searchH.length">
       <div class="search-history-text" >
         <span >历史记录</span>
@@ -26,6 +32,7 @@
         </li>
       </ul>
     </div>
+    <!-- 热门搜索 -->
     <div class="search-hot">
       <div class="search-hot-text">热门搜索</div>
       <ul class="search-hot-list">
@@ -39,12 +46,19 @@
       </ul>
     </div>
   </div>
+  <div v-show="searchVal">
+    <KeywordList :searchVal="searchVal"></KeywordList>
+  </div>
+      </div>
+  </div>
 </template>
 
 <script setup>
-import { onMounted, ref, reactive } from "vue";
+
+import { onMounted, ref } from "vue";
 import { reqSearchInit } from "@/api/search";
 import { Search } from "@element-plus/icons-vue";
+import KeywordList from "./KeywordList.vue"
 let searchVal = ref("");
 let searchHotList = ref([]);
 let searchH = ref([]);
@@ -86,6 +100,9 @@ const searcHistory = (e) => {
 let deleHistory=()=>{
   searchH.value=[];
   localStorage.setItem("searchl", JSON.stringify(searchH.value));
+}
+let handleSearchInput=()=>{
+
 }
 </script>
 
